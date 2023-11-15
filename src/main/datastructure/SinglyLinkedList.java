6798ee45c9f78e953ad2a133fb698c667a243308
ErwinPrograms <erwinprograms@gmail.com>
2023-11-15 13:34:25 -0800

    Add List interface and skeleton methods

--- src/main/datastructure/SinglyLinkedList.java
+++ src/main/datastructure/SinglyLinkedList.java
@@ -1,6 +1,11 @@
 package main.datastructure;
 
-public class SinglyLinkedList<T> {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class SinglyLinkedList<T> implements List<T> {
     private class Node<T> {
         private T data;
         private Node<T> nextNode;
@@ -37,12 +42,118 @@ public class SinglyLinkedList<T> {
         this.head = new Node<>(initialNodeData);
     }
 
+    @Override
     public T get(int index) {
         //placeholder
         return head.getData();
     }
 
+    @Override
+    public T set(int i, T t) {
+        return null;
+    }
+
+    @Override
+    public void add(int i, T t) {
+
+    }
+
+    @Override
+    public T remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int i) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
     public boolean isEmpty() {
         return head == null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] t1s) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int i, Collection<? extends T> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
     }
 }
