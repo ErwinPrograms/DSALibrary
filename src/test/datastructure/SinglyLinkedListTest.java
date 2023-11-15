eacdaf22e9033aae0c8804fd5e82894dee0520b1
ErwinPrograms <erwinprograms@gmail.com>
2023-11-15 13:47:31 -0800

    Add test for .add() method

--- src/test/datastructure/SinglyLinkedListTest.java
+++ src/test/datastructure/SinglyLinkedListTest.java
@@ -1,14 +1,23 @@
 package test.datastructure;
 
 import main.datastructure.SinglyLinkedList;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SinglyLinkedListTest {
 
-    @Test
-    public void defaultConstructor() {
-        SinglyLinkedList<String> emptyList = new SinglyLinkedList<>();
-        assert emptyList.isEmpty();
+    private SinglyLinkedList<String> testList;
+
+    @Before
+    public void setup() {
+        this.testList = new SinglyLinkedList<>();
+        assert testList.isEmpty();
+    }
+
+    @After
+    public void cleanup() {
+        this.testList.clear();
     }
 
     @Test
@@ -16,5 +25,10 @@ public class SinglyLinkedListTest {
         SinglyLinkedList<String> stringList = new SinglyLinkedList<>("Hello");
         assert !stringList.isEmpty();
         assert stringList.get(0).equals("Hello");
+    }
+
+    @Test
+    public void addTest() {
+        assert this.testList.add("first");
     }
 }
